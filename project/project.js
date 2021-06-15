54c090a26a32a56ec5e3a7ed4352ba776d7e5c74
tdt2005 <80700981+tdt2005@users.noreply.github.com>
2021-06-15 18:26:56 +0700

    Add files via upload

---
+++ project/project.js
@@ -0,0 +1,21 @@
+function scroll1() {
+    // window.scroll({0,1681, behavior})
+    window.scrollTo({
+        top: 1682,
+        behavior: 'smooth'
+      })
+}
+
+function scroll2() {
+    window.scrollTo({
+        top: 730,
+        behavior: 'smooth'
+      })
+}
+
+function scroll3() {
+    window.scrollTo({
+        top: 2649,
+        behavior: 'smooth'
+      })
+}
